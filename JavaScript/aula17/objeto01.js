56fe9301ae863de827d2f553e6280e0baf339aca
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-10 11:05:05 -0300

    Um pouquinho sobre objetos em JS

---
+++ JavaScript/aula17/objeto01.js
@@ -0,0 +1,14 @@
+let amigo = {
+    nome: 'José',
+    sexo: 'M',
+    peso: 85.4,
+    engordar(p=0) {
+        console.log('Engordou!');
+        this.peso += p;
+    }
+};
+
+console.log(`${amigo.nome} pesa ${amigo.peso} kg`);
+
+amigo.engordar(2.5);
+console.log(`${amigo.nome} pesa ${amigo.peso} kg`);
